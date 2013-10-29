c7e64ea17b49603c133d6c0fc8969794230527ee
porlin72 <porlin72@yahoo.com>
2013-10-28 22:30:03 -0400

    Update models.py

--- contacts/models.py
+++ contacts/models.py
@@ -6,6 +6,10 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=25)
+    city = models.CharField(max_length=15)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5)
     
     def __unicode__(self):
         return "%s %s" % (self.first_name, self.last_name)
